9cf2035c95904cb1eee4bb2a37d47dc5a626b85e
AlwinaO <aoyewole2586@gmail.com>
2019-09-29 22:34:36 -0400

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ class Driver {
     }
 
     yearsExperienceFromBeginningOf(year) {
-        
+        const startYear = this.startDate.getFullYear();
+        return year - startYear;
+    }
+}
+
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = { beginningLocation };
+        this.endingLocation = { endingLocation };
+    }
+
+    blocksTravelled() {
+        let eastWest = [
+            '1st Avenue',
+            '2nd Avenue',
+            '3rd Avenue',
+            'Lexington Avenue',
+            'Park',
+            'Madison Avenue',
+            '5th Avenue'
+        ];
+        if (eastWest < eastWest) {
+            return this.endingLocation[endingLocation] - this.beginningLocation[beginningLocation];
+        } else if (eastWest > eastWest) {
+            return this.beginningLocation - this.endingLocation;
+        } else {
+            return this.beginningLocation + this.endingLocation;
+        }
     }
 }
